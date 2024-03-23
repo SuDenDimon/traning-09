315f1e498ee5a171ed9ea354641dcd24ca3027dc
tina3008 <kicja_3@ukr.net>
2024-03-23 13:13:30 +0200

    task-5

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -1,6 +1,8 @@
 export class UnsplashAPI {
     #BASE_URL = "https://api.unsplash.com/search/photos";
     #API_KEY = "gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58";
+    #query = "";
+    
 
     getPopularsFhotos(page) {
         const url = `${this.#BASE_URL}?query=popular&page=${page}&per_page=12&orientation=portrait&client_id=${this.#API_KEY}`
@@ -11,5 +13,16 @@ export class UnsplashAPI {
             }
             return response.json();
         }).catch(err => console.log("error"))
+    };
+    getFhotosByQuery(page) {
+        const url = `${this.#BASE_URL}?query=${this.#query}&page=${page}&per_page=12&orientation=portrait&client_id=${this.#API_KEY}`
+
+        return fetch(url).then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        }).catch(err => console.log("error"))
     }
+set query(newQuery){this.#query=newQuery}
 }
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,6 +3,8 @@ import { createGalleryCard } from "./createGalleryCard";
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
 
+const searchForm = document.querySelector(".js-search-form")
+
 const gallery = document.querySelector(".js-gallery");
 const service = new UnsplashAPI();
 
@@ -24,3 +26,38 @@ service.getPopularsFhotos(page).then(({results, total}) => {
     gallery.innerHTML = markup;
 } );
 
+pagination.on('afterMove',popular );
+function popular (event){
+    const currentPage = event.page;
+    service.getPopularsFhotos(currentPage).then(({results, total}) => {
+        const markup = createGalleryCard(results);
+        gallery.innerHTML = markup;
+    } );
+}
+searchForm.addEventListener('submit',onSearchPhotos)
+
+function onSearchPhotos(event){
+event.preventDefault()
+const searchQuery = event.target.elements.query.value.trim();
+if (!searchQuery) return
+service.query=searchQuery
+pagination.off('afterMove',popular );
+pagination.off('afterMove',byQuery );
+service.getFhotosByQuery(page)
+.then(({results, total}) => {
+    if (results.length===0) return;
+    const markup = createGalleryCard(results);
+    
+    pagination.reset(total);
+    gallery.innerHTML = markup;
+} );
+pagination.on('afterMove',byQuery );
+}
+
+function byQuery (event){
+    const currentPage = event.page;
+    service.getFhotosByQuery(currentPage).then(({results, total}) => {
+        const markup = createGalleryCard(results);
+        gallery.innerHTML = markup;
+    } );
+}
