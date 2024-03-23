9d23ad41ef57c3183772374e3c6e1af59e83e551
Yana17-ratsa <ratsayana13@gmail.com>
2024-03-23 12:12:00 +0200

    task-3

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -1,9 +1,9 @@
 export class UnsplashAPI {
-    _BASE_URL = "https: //api.unsplash.com/search/photos";
-    _API_KEY = "gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58";
+    #BASE_URL = "https://api.unsplash.com/search/photos";
+    #API_KEY = "gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58";
 
     getPopularsFhotos(page) {
-        const url = `${this._BASE_URL}?query=popular&page=${page}&per_page=12&orientation=portrait&client_id=${this._API_KEY}`
+        const url = `${this.#BASE_URL}?query=popular&page=${page}&per_page=12&orientation=portrait&client_id=${this.#API_KEY}`
 
         return fetch(url).then((response) => {
             if (!response.ok) {
