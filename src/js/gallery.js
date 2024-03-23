4bfacdd1f98417e8e8efe9e52b9dc05739ed7490
SuDenDimon <94171603+SuDenDimon@users.noreply.github.com>
2024-03-23 12:35:14 +0200

    task-4

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,12 +1,26 @@
 import {UnsplashAPI} from "./UnsplashAPI";
 import { createGalleryCard } from "./createGalleryCard";
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
 
 const gallery = document.querySelector(".js-gallery");
 const service = new UnsplashAPI();
 
-service.getPopularsFhotos(1).then((data) => {
-    const markup = createGalleryCard(data.results);
+const container = document.getElementById('tui-pagination-container');
 
+const options = { // below default value of options
+    totalItems: 0,
+    itemsPerPage: 12,
+    visiblePages: 5,
+    page: 1,
+}
+
+const pagination = new Pagination(container, options);
+const page = pagination.getCurrentPage();
+
+service.getPopularsFhotos(page).then(({results, total}) => {
+    const markup = createGalleryCard(results);
+    pagination.reset(total);
     gallery.innerHTML = markup;
 } );
 
